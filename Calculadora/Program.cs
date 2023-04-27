090441b1d3990526e5a05c15db561268f0026e40
philipheferreira <philipheferreira@gmail.com>
2023-04-27 11:26:11 -0300

    update calc doc a parte

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,23 +1,7 @@
-﻿
+﻿using System;
 
 namespace principal { 
 
-    class calculadora{  
-        public int soma(int x, int y) { 
-        return x + y;
-        }
-        public int sub(int x, int y) {
-            return x - y;
-        }
-
-        public int mult(int x, int y) { 
-            return x * y;
-        }
-
-        public float div(float x, float y) {
-            return x / y;
-        }
-    }
     public class Program {  
         static void Main(string[] args) {
 
---
+++ Calculadora/calc.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace principal { 
+
+    class calculadora{  
+        public int soma(int x, int y) { 
+        return x + y;
+        }
+        public int sub(int x, int y) {
+            return x - y;
+        }
+
+        public int mult(int x, int y) { 
+            return x * y;
+        }
+
+        public float div(float x, float y) {
+            return x / y;
+        }
+    }
+
+}
