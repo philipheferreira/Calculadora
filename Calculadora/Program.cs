c843cfe9a6bc28485f643429b15b510093e02aac
philipheferreira <philipheferreira@gmail.com>
2023-04-27 15:43:04 -0300

    update menu

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -4,16 +4,36 @@ namespace principal {
 
     public class Program {  
         static void Main(string[] args) {
-
+            int menu;
             calculadora meuObjt1 = new calculadora();
 
-            Console.WriteLine(meuObjt1.soma(20, 40));
+            do {
+                Console.WriteLine("####---Calculadora---#######");
 
-            Console.WriteLine(meuObjt1.sub(20, 40));
+                Console.WriteLine("1- somar");
 
-            Console.WriteLine(meuObjt1.mult(20, 40));
+                Console.WriteLine("2-subtrair");
 
-            Console.WriteLine(meuObjt1.div(20, 40));
+                Console.WriteLine("3-multiplicar");
+
+                Console.WriteLine("4-dividir");
+
+                Console.WriteLine("0-sair");
+
+                menu = Convert.ToInt32(Console.ReadLine());
+
+                if(menu == 1) { 
+                    Console.WriteLine(meuObjt1.soma(20, 40));
+                } else if(menu == 2) { 
+                    Console.WriteLine(meuObjt1.sub(20, 40));
+                } else if(menu == 3) { 
+                    Console.WriteLine(meuObjt1.mult(20, 40));
+                } else if(menu == 4) { 
+                    Console.WriteLine(meuObjt1.div(20, 40));
+                } else {
+                    Console.WriteLine("Voce colocou uma informacao incorreta");
+                }
+            } while (menu != 0);
         }
     }
 
